443605640175b2b94a48568082aa307b02ce6d04
Pengfei Tian (Leo) <tianpengfei.leo@gmail.com>
2022-01-09 19:30:11 +0800

    feat: add remote-redux-devtools

--- app/src/navigators/MainStackNavigator/index.js
+++ app/src/navigators/MainStackNavigator/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AppTabNavigator from '../AppTabNavigator';
 import AuthStackNavigator from '../AuthStackNavigator';
@@ -9,8 +9,8 @@ const Stack = createNativeStackNavigator();
 const MainStackNavigator = () => {
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen name="Auth" component={AuthStackNavigator} options={{headerShown: false}} /> */}
-      <Stack.Screen name="App" component={AppTabNavigator} options={{headerShown: false}} />
+      <Stack.Screen name="Auth" component={AuthStackNavigator} options={{ headerShown: false }} />
+      <Stack.Screen name="App" component={AppTabNavigator} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
