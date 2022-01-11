6a23b5dbd965fe60b7aeb2e06172734a3733c0ce
Pengfei Tian (Leo) <tianpengfei.leo@gmail.com>
2022-01-11 15:08:24 +0800

    feat: add basic code for mock api

--- app/App.js
+++ app/App.js
@@ -11,6 +11,8 @@ import type { Node } from 'react';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { createServer } from 'miragejs';
+
 import configStore from './src/store/configStore';
 import RootStackNavigator from './src/navigators/RootStackNavigator';
 
@@ -18,13 +20,50 @@ import ThemeProvider from './src/contexts/ThemeProvider';
 
 const store = configStore();
 
+import { View, Text } from 'react-native';
+
+if (window.server) {
+  console.log(`window.server: ${window.server}`);
+  console.log(`server: ${window.server}`);
+  server.shutdown();
+}
+
+window.server = createServer({
+  routes() {
+    this.get('/api/movies', () => {
+      return {
+        movies: [
+          { id: 1, name: 'Inception', year: 2010 },
+          { id: 2, name: 'Interstellar', year: 2014 },
+          { id: 3, name: 'Dunkirk', year: 2017 },
+        ],
+      };
+    });
+  },
+});
+
 const App: () => Node = () => {
+  const [movies, setMovies] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/api/movies')
+      .then((res) => res.json())
+      .then((json) => setMovies(json.movies));
+  }, []);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <ThemeProvider>
+        <View>
+          {movies.map((movie) => (
+            <Text key={movie.id}>
+              {movie.name} ({movie.year})
+            </Text>
+          ))}
+        </View>
+        {/* <ThemeProvider>
           <RootStackNavigator />
-        </ThemeProvider>
+        </ThemeProvider> */}
       </SafeAreaProvider>
     </Provider>
   );
